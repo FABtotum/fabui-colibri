af68af421a7a090fb32b102c28d2ea4d0581278f
Simone Cociancich <sc@fabtotum.com>
2015-04-16 16:22:23 +0200

    Corrected raspi cam endpoint for handling file permissions correctly.

--- fabui/application/widgets/cam/ajax/picture.php
+++ fabui/application/widgets/cam/ajax/picture.php
@@ -1,17 +1,16 @@
 <?php
-require_once $_SERVER['DOCUMENT_ROOT'].'/lib/config.php';
-require_once $_SERVER['DOCUMENT_ROOT'] .'/lib/utilities.php';
-//raspistill -hf -vf -rot 90  --exposure off -awb sun -ISO 400 -w 768 -h 1024 -o file.jpg  -t 0
-$_image   = '/var/www/temp/picture.jpg';
 
-if(!file_exists($_image)){
-	write_file($_image, '', 'w');
-}
-chmod($_image, 0777);
+require_once $_SERVER['DOCUMENT_ROOT'].'/lib/config.php';
+require_once $_SERVER['DOCUMENT_ROOT'].'/lib/utilities.php';
+
+$_image   = '/var/www/temp/picture.jpg';
 $_command = 'sudo raspistill -n -hf -t 1 -rot 90 -awb sun -ISO 800 -w 768 -h 1024 -o '.$_image;
 shell_exec ( $_command );
-//sleep(6);
+shell_exec ('sudo chgrp www-data '.$_image);
+shell_exec ('sudo chmod ug+rw '.$_image);
+
 $_response_items['command'] = $_command;
 header('Content-Type: application/json');
 echo json_encode($_response_items);
+
 ?>
