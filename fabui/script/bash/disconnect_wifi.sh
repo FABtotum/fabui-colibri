4600da42627220ba403addde9f222e230b9831c0
Krios <krios.mane@gmail.com>
2016-04-07 10:07:17 +0100

    fixed wpa_supplicant.conf path

--- fabui/script/bash/disconnect_wifi.sh
+++ fabui/script/bash/disconnect_wifi.sh
@@ -2,9 +2,9 @@
 
 CONFIG="ctrl_interface=DIR=/run/wpa_supplicant GROUP=netdev\nupdate_config=1\n"
 
-sudo chmod 666 /etc/wpa_supplicant/wpa_supplicant.conf
-echo -e $CONFIG > /etc/wpa_supplicant/wpa_supplicant.conf
-sudo chmod 644 /etc/wpa_supplicant/wpa_supplicant.conf
+sudo chmod 666 /etc/wpa_supplicant.conf
+echo -e $CONFIG > /etc/wpa_supplicant.conf
+sudo chmod 644 /etc/wpa_supplicant.conf
 
 sudo ifdown wlan0
 sudo ifup wlan0
