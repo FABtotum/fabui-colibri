398180e736611d5d18616d5ca72e193f40e228ff
Daniel Kesler <kesler.daniel@gmail.com>
2016-10-06 14:54:48 +0200

    - added: support view

--- fabui/application/controllers/Support.php
+++ fabui/application/controllers/Support.php
@@ -12,9 +12,28 @@
  	
 	public function index()
 	{
+		//load libraries, helpers, model
+		$this->load->library('smart');
+		$this->load->helper('form');
+		//data
+		$data = array();
 		
+		//main page widget
+		$widgetOptions = array(
+			'sortable'     => false, 'fullscreenbutton' => true,  'refreshbutton' => false, 'togglebutton' => false,
+			'deletebutton' => false, 'editbutton'       => false, 'colorbutton'   => false, 'collapsed'    => false
+		);
+		
+		$widget         = $this->smart->create_widget($widgetOptions);
+		$widget->id     = 'main-widget-4th-axis';
+		$widget->header = array('icon' => 'fa-support ', "title" => "<h2>Support</h2>");
+		$widget->body   = array('content' => $this->load->view('support/widget', $data, true ), 'class'=>'fuelux');
+		
+		$this->addJsInLine($this->load->view('support/js', $data, true));
+		$this->content = $widget->print_html(true);
+		$this->view();
 	}
-			
+
  }
  
 ?>
---
+++ fabui/application/views/support/widget.php
@@ -0,0 +1,34 @@
+
+<div class="row margin-bottom-10" id="faq-content"></div>
+
+<div class="row margin-bottom-10">
+	<div class="col-sm-4">
+		<div class="well  well-light">
+			<h2 class="text-center"><a style="cursor:pointer;" target="_blank" href="http://wiki.fabtotum.com/doku.php"><i class="fa fa-wikipedia-w fa-border fa-2x"></i></a></h2>
+			<p class="text-center">Access the FABtotum Wiki</p>
+		</div>
+	</div>
+	<div class="col-sm-4">
+		<div class="well  well-light">
+			<h2 class="text-center"><a style="cursor:pointer;" target="_blank" href="http://support.fabtotum.com/manual_eng.pdf"><i class="fa fa-book fa-border fa-2x"></i></a></h2>
+			<p class="text-center">First Setup</p>
+		</div>
+	</div>
+	<div class="col-sm-4">
+		<div class="well  well-light">
+			<h2 class="text-center"><a style="cursor:pointer;" target="_blank" href="https://github.com/FABtotum/FAB_Configs/archive/master.zip"><i class="fa fa-cog fa-border fa-2x"></i></a></h2>
+			<p class="text-center">Download the latest slicing configurations</p>
+		</div>
+	</div>
+</div>
+
+<div class="row margin-bottom-10">
+	<div class="col-sm-12">
+		<div class="well  well-light">
+			<h2 class="text-center"><a style="cursor:pointer;" target="_blank" href="http://forum.fabtotum.com/"><i class="fa fa-comments fa-border fa-2x"></i></a></h2>
+			<h2 class="text-center">Support Communities</h2>
+			<p class="text-center">Find and share solutions with fellow Fabtotum users around the world</p>
+			<p class="text-center"><a href="http://forum.fabtotum.com/" target="_blank">Join the conversation</a></p>
+		</div>
+	</div>
+</div>
