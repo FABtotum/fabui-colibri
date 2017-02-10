d50c179c2f7adec9afc89a446f3e66c82d3abb7b
Krios <krios.mane@gmail.com>
2017-02-10 13:08:57 +0100

    updated support module

---
+++ fabui/application/views/support/index.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * 
+ * @author Krios Mane
+ * @version 0.1
+ * @license https://opensource.org/licenses/GPL-3.0
+ * 
+ */
+ 
+?>
+<section id="widget-grid">
+	<div class="row">
+		<div class="col-sm-12"><?php echo $supportWidget;?></div>
+	</div>
+</section>
--- fabui/application/views/support/widget.php
+++ fabui/application/views/support/widget.php
@@ -1,34 +1,43 @@
-
-<div class="row margin-bottom-10" id="faq-content"></div>
-
-<div class="row margin-bottom-10">
-	<div class="col-sm-4">
-		<div class="well  well-light">
-			<h2 class="text-center"><a class="no-ajax"  style="cursor:pointer;" target="_blank" href="http://wiki.fabtotum.com/doku.php"><i class="fa fa-wikipedia-w fa-border fa-2x"></i></a></h2>
-			<p class="text-center">Access the FABtotum Wiki</p>
+<?php
+/**
+ * 
+ * @author Krios Mane
+ * @version 0.1
+ * @license https://opensource.org/licenses/GPL-3.0
+ * 
+ */
+?>
+<div class="row">
+	<div class="col-sm-6 col-xs-6 margin-bottom-10">
+		<div class="well well-sm  text-center min-height-200">
+			<a href="http://www.fabtotum.com/wp-content/uploads/2016/10/Quick-Start-Guide-EN10.2.pdf" target="_blank" class="btn btn-default btn-circle btn-xl margin-top-10 no-ajax"><i class="fa fa-book"></i></a>
+			<h3>Quick start guide</h3>
+			<p class="">This quick guide is a must read for getting started!</p>
 		</div>
 	</div>
-	<div class="col-sm-4">
-		<div class="well  well-light">
-			<h2 class="text-center"><a class="no-ajax"  style="cursor:pointer;" target="_blank" href="http://support.fabtotum.com/manual_eng.pdf"><i class="fa fa-book fa-border fa-2x"></i></a></h2>
-			<p class="text-center">First Setup</p>
+	
+	<div class="col-sm-6 col-xs-6 col-xs-6 margin-bottom-10">
+		<div class="well well-sm  text-center min-height-200">
+			<a href="http://www.fabtotum.com/knowledgebase/" target="_blank" class="btn btn-default btn-circle btn-xl margin-top-10 no-ajax"><i class="fa fa-eye"></i></a>
+			<h3>Knowledgebase</h3>
+			<p class="">All that you need to know is in the Knowledgebase</p>
 		</div>
 	</div>
-	<div class="col-sm-4">
-		<div class="well  well-light">
-			<h2 class="text-center"><a class="no-ajax"  style="cursor:pointer;" target="_blank" href="https://github.com/FABtotum/FAB_Configs/archive/master.zip"><i class="fa fa-cog fa-border fa-2x"></i></a></h2>
-			<p class="text-center">Download the latest slicing configurations</p>
+	
+	<div class="col-sm-6 col-xs-6 col-xs-6 margin-bottom-10">
+		<div class="well well-sm  text-center min-height-200">
+			<a href="https://github.com/FABtotum/FAB_Configs/archive/master.zip" target="_blank" class="btn btn-default btn-circle btn-xl margin-top-10 no-ajax"><i class="fa fa-cogs"></i></a>
+			<h3>Slicing profiles  & configurations</h3>
+			<p class="">A public archive with the latest profiles for most commonly used slicing </p>
+		</div>
+	</div>
+	
+	<div class="col-sm-6 col-xs-6 col-xs-6 margin-bottom-10 ">
+		<div class="well well-sm  text-center min-height-200">
+			<a href="http://www.fabtotum.com/tickets" target="_blank" class="btn btn-default btn-circle btn-xl margin-top-10 no-ajax"><i class="fa fa-comments"></i></a>
+			<h3>Support</h3>
+			<p class="">Get in touch with our technical support team</p>
 		</div>
 	</div>
 </div>
-
-<div class="row margin-bottom-10">
-	<div class="col-sm-12">
-		<div class="well  well-light">
-			<h2 class="text-center"><a style="cursor:pointer;" target="_blank" href="http://forum.fabtotum.com/"><i class="fa fa-comments fa-border fa-2x"></i></a></h2>
-			<h2 class="text-center">Support Communities</h2>
-			<p class="text-center">Find and share solutions with fellow Fabtotum users around the world</p>
-			<p class="text-center"><a class="no-ajax"  href="http://forum.fabtotum.com/" target="_blank">Join the conversation</a></p>
-		</div>
-	</div>
-</div>
+<hr class="simple">
