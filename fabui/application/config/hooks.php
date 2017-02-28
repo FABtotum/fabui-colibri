66348ca26322ee95763d3cd2b0894514af39189d
krios-fabteam <km@fabtotum.com>
2017-02-28 17:24:53 +0100

    possible session lock fix

--- fabui/application/config/hooks.php
+++ fabui/application/config/hooks.php
@@ -11,3 +11,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |	https://codeigniter.com/user_guide/general/hooks.html
 |
 */
+
+$hook['post_controller'] = function()
+{
+	session_write_close();
+};
