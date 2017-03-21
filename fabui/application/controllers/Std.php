b8be1f26a262f682881dfe1e22dc04035bad0a82
krios-fabteam <km@fabtotum.com>
2017-03-21 11:13:12 +0100

    std views list files changed icons

--- fabui/application/controllers/Std.php
+++ fabui/application/controllers/Std.php
@@ -78,8 +78,8 @@ class Std extends FAB_Controller {
 		$aaData = array();
 		foreach($data as $file){ 
 			$td0 = '<label class="radio"><input type="radio" name="create-file" value="'.$file['id_file'].'"><i></i></label>';
-			$td1 = '<i></i><span class="hidden-xs">'.$file['client_name'].'</span><span class="hidden-md hidden-sm hidden-lg">'.ellipsize($file['orig_name'], 35).'</span>';
-			$td2 = '<i class="fa fa-folder-open"></i> <span class="hidden-xs">'.$file['name'].'</span><span class="hidden-md hidden-sm hidden-lg">'.ellipsize($file['name'], 35).'</span>';
+			$td1 = '<i class="fa fa-cube hidden-xs"></i> <span class="hidden-xs">'.$file['client_name'].'</span><span class="hidden-md hidden-sm hidden-lg">'.ellipsize($file['orig_name'], 35).'</span>';
+			$td2 = '<i class="fa fa-cubes"></i> <span class="hidden-xs">'.$file['name'].'</span><span class="hidden-md hidden-sm hidden-lg">'.ellipsize($file['name'], 35).'</span>';
 			$td3 = $file['id_file'];
 			$td4 = $file['id_object'];
 			$aaData[] = array($td0, $td1, $td2, $td3, $td4);
