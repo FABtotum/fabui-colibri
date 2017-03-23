75a2a6eeb4582befe6603b7e893714bbcd81f475
krios-fabteam <km@fabtotum.com>
2017-03-23 10:23:49 +0100

    bed calibration revamped to wizard style

---
+++ fabui/application/views/bed/js.php
@@ -0,0 +1,74 @@
+<?php
+/**
+ * 
+ * @author Krios Mane
+ * @author Fabtotum Development Team
+ * @version 0.1
+ * @license https://opensource.org/licenses/GPL-3.0
+ * 
+ */
+?>
+<script type="text/javascript">
+
+	var numProbes = 1;
+	var skipHoming = 0;
+	
+	$(document).ready(function() {
+		init();
+	});
+	
+	/**
+	*
+	**/
+	function handleStep()
+	{
+		var step = $('#myWizard').wizard('selectedItem').step;
+		switch(step){
+			case 1:
+			case 2:
+				doBedCalibration();
+				break;
+		}
+	}
+	/**
+	*
+	**/
+	function checkWizard()
+	{	
+	}
+	/**
+	*
+	*/
+	function init()
+	{
+		enableButton('.button-next');
+	}
+	/**
+	*
+	**/
+	function doBedCalibration()
+	{
+		openWait('<i class="fa fa-spinner fa-spin"></i> <?php echo _("Calibration in progress");?>');
+		$.ajax({
+            type: "POST",
+            url: "<?php echo site_url("bed/calibrate") ?>/"
+                + numProbes + "/"
+                + skipHoming,
+            dataType: "json"
+        }).done(function( data ) {
+			processResponse(data);
+        });
+	}
+	/**
+	*
+	**/
+	function processResponse(data)
+	{
+		numProbes++;
+		skipHoming = 1;
+		$("#calbration-result").html(data.html);
+		closeWait();
+		gotoWizardStep(2);
+		$(".button-next").html("<span>Calibrate again</span>");
+	}
+</script>
