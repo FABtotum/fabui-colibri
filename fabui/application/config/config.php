aec8908942b9e10772ea3d8a183fae3d6c796762
krios-fabteam <km@fabtotum.com>
2017-04-14 12:22:22 +0200

    increased session expiration time to 8 hours

--- fabui/application/config/config.php
+++ fabui/application/config/config.php
@@ -368,7 +368,7 @@ $config['encryption_key'] = '4KdxvL3hYxv4xu9Uwk4fxCvP7AIPlBX7';
 */
 $config['sess_driver'] = 'files';
 $config['sess_cookie_name'] = 'ci_session';
-$config['sess_expiration'] = 7200;
+$config['sess_expiration'] = 28800; //8 hours
 $config['sess_save_path'] = '/tmp/'; 
 $config['sess_match_ip'] = FALSE;
 $config['sess_time_to_update'] = 300;
