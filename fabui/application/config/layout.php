e515eaf2e27a1301b2bb4e1ae466b8328b68f362
krios-fabteam <km@fabtotum.com>
2017-08-30 17:36:43 +0200

    fixed missing js tours

--- fabui/application/config/layout.php
+++ fabui/application/config/layout.php
@@ -22,13 +22,13 @@ $config['javascript']['mandatory'] = array(
 	'/assets/js/fab.app.js',
 	'/assets/js/plugin/bootstrap-tour/bootstrap-tour.js',
 	'/assets/js/fabtotum.js',
-//	'/assets/js/tours/0010_head.js',
-//	'/assets/js/tours/0020_bed.js',
-//	'/assets/js/tours/0030_nozzle_assisted.js',
-//	'/assets/js/tours/0031_nozzle_fine.js',
-//	'/assets/js/tours/0040_spool_load.js',
-//	'/assets/js/tours/0041_spool_unload.js',
-//	'/assets/js/tours/0050_feeder.js'
+	'/assets/js/tours/0010_head.js',
+	'/assets/js/tours/0020_bed.js',
+	'/assets/js/tours/0030_nozzle_assisted.js',
+	'/assets/js/tours/0031_nozzle_fine.js',
+	'/assets/js/tours/0040_spool_load.js',
+	'/assets/js/tours/0041_spool_unload.js',
+	'/assets/js/tours/0050_feeder.js'
 );
 /***
  * 
