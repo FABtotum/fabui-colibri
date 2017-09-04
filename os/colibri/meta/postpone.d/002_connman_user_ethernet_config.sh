fa2c412c2f617f9fbb6b502c6c5342a685815e05
Daniel Kesler <dk@fabtotum.com>
2017-09-04 11:50:15 +0200

    Connman upgrade script fix #3

--- os/colibri/meta/postpone.d/002_connman_user_ethernet_config.sh
+++ os/colibri/meta/postpone.d/002_connman_user_ethernet_config.sh
@@ -21,6 +21,8 @@ source /usr/share/fabui/ext/bash/connman_nm_functions.sh
 # Ethernet configuration upgrade
 EOF
 
+	chmod +x ${CONNMAN_UPGRADE_FILE}
+
 	SERVICE=$(connman_iface2service eth0)
 	SETTINGS_FILE="${CONNMAN_SERVICES_DIR}/${SERVICE}/settings"
 	IPv4=$(echo $NETWORK_IPV4 | awk -F / '{print $1}')
