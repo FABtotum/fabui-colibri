a4e644ae67e9eb47896dbf4cbc30b5d12984bfb9
Daniel Kesler <dk@fabtotum.com>
2017-09-04 12:18:27 +0200

    connman upgrade script fix #4

--- os/colibri/meta/postpone.d/002_connman_user_ethernet_config.sh
+++ os/colibri/meta/postpone.d/002_connman_user_ethernet_config.sh
@@ -14,14 +14,14 @@ if [ -f "${CONNMAN_SERVICES_DIR}/${CONNMAN_ETH_CONFIG_FILE}" ]; then
 else
 	mkdir -p $(dirname $CONNMAN_UPGRADE_FILE)
 	
-cat <<EOF > ${CONNMAN_UPGRADE_FILE}
-# Automatically generated upgrade file, do not edit \n
-source /usr/share/fabui/ext/bash/connman_nm_functions.sh
+#~ cat <<EOF > ${CONNMAN_UPGRADE_FILE}
+#~ # Automatically generated upgrade file, do not edit \n
+#~ source /usr/share/fabui/ext/bash/connman_nm_functions.sh
 
-# Ethernet configuration upgrade
-EOF
+#~ # Ethernet configuration upgrade
+#~ EOF
 
-	chmod +x ${CONNMAN_UPGRADE_FILE}
+	#~ chmod +x ${CONNMAN_UPGRADE_FILE}
 
 	SERVICE=$(connman_iface2service eth0)
 	SETTINGS_FILE="${CONNMAN_SERVICES_DIR}/${SERVICE}/settings"
@@ -33,11 +33,11 @@ EOF
 	if [ -e "$SETTINGS_FILE" ]; then
 		echo "Reading settings from file"
 		# fallback, read addresses from settings
-		export METHOD=$(cat "$SETTINGS_FILE" |  grep "IPv4.method" | awk -F= '{print $2}')
-		export NETMASK_PREFIX=$(cat "$SETTINGS_FILE" |  grep "IPv4.netmask_prefixlen" | awk -F= '{print $2}')
-		export IPv4=$(cat "$SETTINGS_FILE" |  grep "IPv4.local_address" | awk -F= '{print $2}')
-		export GATEWAY=$(cat "$SETTINGS_FILE" |  grep "IPv4.gateway" | awk -F= '{print $2}')
-		
+		METHOD=$(cat "$SETTINGS_FILE" |  grep "IPv4.method" | awk -F= '{print $2}')
+		NETMASK_PREFIX=$(cat "$SETTINGS_FILE" |  grep "IPv4.netmask_prefixlen" | awk -F= '{print $2}')
+		IPv4=$(cat "$SETTINGS_FILE" |  grep "IPv4.local_address" | awk -F= '{print $2}')
+		GATEWAY=$(cat "$SETTINGS_FILE" |  grep "IPv4.gateway" | awk -F= '{print $2}')
+		NS="Nameservers = $(connman_get_dns_tail)"
 		# Generate netmask based on ip/prefix
 		eval $(ipcalc -m $IPv4/$NETMASK_PREFIX)
 		
@@ -45,16 +45,32 @@ EOF
 		
 		case $METHOD in
 			fixed|manual)
-				echo "config_ethernet_static eth0 $IPv4 $NETMASK $GATEWAY" >> ${CONNMAN_UPGRADE_FILE}
+				#echo "config_ethernet_static eth0 $IPv4 $NETMASK $GATEWAY" >> ${CONNMAN_UPGRADE_FILE}
 				#~ echo "config_ethernet_static eth0 $IPv4 $NETMASK $GATEWAY"
+cat <<EOF > ${CONNMAN_SERVICES_DIR}/${CONNMAN_ETH_CONFIG_FILE}
+# Automatically generated, do not edit \n
+[service_ethernet]
+Type = ethernet
+IPv4 = $IPv4/$NETMASK/$GATEWAY
+$NS
+IPv6 = Off
+EOF
 				;;
 			dhcp)
-				echo "config_ethernet_dhcp eth0" >> ${CONNMAN_UPGRADE_FILE}
-				#~ echo "config_ethernet_dhcp eth0"
+cat <<EOF > ${CONNMAN_SERVICES_DIR}/${CONNMAN_ETH_CONFIG_FILE}
+# Automatically generated, do not edit \n
+[service_ethernet_wifi]
+Type = ethernet
+IPv4 = dhcp
+IPv6 = Off
+EOF
 				;;
 		esac
 		
-		echo "rm -rf ${CONNMAN_SERVICES_DIR}/${SERVICE}" >> ${CONNMAN_UPGRADE_FILE}
+		rm -rf ${CONNMAN_SERVICES_DIR}/${SERVICE}
+		/etc/init.d/connman restart
+		
+		#echo "rm -rf ${CONNMAN_SERVICES_DIR}/${SERVICE}" >> ${CONNMAN_UPGRADE_FILE}
 		
 	fi
 fi
