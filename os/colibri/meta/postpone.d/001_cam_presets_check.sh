40a9d6aed3ad6a0ce1b6fe4c5e16ca2ff4993b08
krios-fabteam <km@fabtotum.com>
2017-11-08 12:15:37 +0100

    fixed cam toolbox upgrade script

--- os/colibri/meta/postpone.d/001_cam_presets_check.sh
+++ os/colibri/meta/postpone.d/001_cam_presets_check.sh
@@ -6,6 +6,10 @@
 
 CAM_001_CHECK_FILE="/var/lib/fabui/upgrade.d/001_cam.sh"
 
+CAM_USERDATA_FOLDER="/mnt/userdata/cam"
+CAM_FABUI_FOLDER="/usr/share/fabui/cam"
+LIB_PATH="/var/lib"
+
 echo "CAM toolbox presets check #001 preparation"
 
 mkdir -p $(dirname $CAM_001_CHECK_FILE)
@@ -14,16 +18,14 @@ cat <<EOF > ${CAM_001_CHECK_FILE}
 #!/bin/bash
 # Automatically generated upgrade file, do not edit
 
-CAM_USERDATA_FOLDER="/mnt/userdata/cam"
-CAM_FABUI_FOLDER="/usr/share/fabui/cam"
-
 if [[ ! -e $CAM_USERDATA_FOLDER ]] ; then
 	mkdir -p $CAM_USERDATA_FOLDER
 	
 	if [[ -e  $CAM_FABUI_FOLDER ]] ; then
 		cp -R $CAM_FABUI_FOLDER/* $CAM_USERDATA_FOLDER
 		chown -R 33:33 /mnt/userdata/*
-		sudo -uwww-data ln -s /mnt/userdata/cam /var/lib/fabui
+		sudo rm -rvf $LIB_PATH/fabui/cam
+		sudo -uwww-data ln -s /mnt/userdata/cam $LIB_PATH/fabui
 	fi
 fi
 EOF
