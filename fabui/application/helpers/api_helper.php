620e9092f19bbf83be2172e96b56be42fdcfc862
krios-fabteam <km@fabtotum.com>
2017-11-20 18:23:33 +0100

    added get request mode to api helper

--- fabui/application/helpers/api_helper.php
+++ fabui/application/helpers/api_helper.php
@@ -30,7 +30,7 @@ if(!function_exists('call_remote_api'))
 	/**
 	 * make call to remote api url (http://app.fabtotum.com)
 	 */
-	function call_remote_api($endpoint, $data = array())
+	function call_remote_api($endpoint, $data = array(), $post = true)
 	{
 		$CI =& get_instance();
 		$CI->config->load('fabtotum');
@@ -40,8 +40,10 @@ if(!function_exists('call_remote_api'))
 		//prepare call
 		$ch = curl_init($url);
 		curl_setopt($ch, CURLOPT_URL,            $url);
-		curl_setopt($ch, CURLOPT_POST,           true);
-		curl_setopt($ch, CURLOPT_POSTFIELDS,     $data);
+		if($post){
+		  curl_setopt($ch, CURLOPT_POST,           true);
+		  curl_setopt($ch, CURLOPT_POSTFIELDS,     $data);
+		}
 		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 		curl_setopt($ch, CURLOPT_CONNECTTIMEOUT ,0);
 		curl_setopt($ch, CURLOPT_TIMEOUT,        $CI->config->item('connection_timeout'));
